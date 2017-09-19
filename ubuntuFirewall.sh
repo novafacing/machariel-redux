7bef9df07633ddbfaa007f45d11202e885db8200
Rowan Hart <rowanbhart@gmail.com>
2017-09-19 19:36:48 -0400

    Changed rules again, decommented ubuntuFirewall.sh

--- ubuntuFirewall.sh
+++ ubuntuFirewall.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 
-#while getopts "p:" opt; do
-#  case $opt in
-#    p ) set -f
-#        IFS=' '
-#        OPTIONS=($OPTARG);;
-#    * )
-#        clean_up
-#        exit 1
-#  esac
-#done
+while getopts "p:" opt; do
+  case $opt in
+    p ) set -f
+        IFS=' '
+        OPTIONS=($OPTARG);;
+    * )
+        clean_up
+        exit 1
+  esac
+done
 
-#echo $OPTIONS
+echo $OPTIONS
 
-#PORTS=$( IFS=$'\n'; echo "${OPTIONS[@]}")
-#PORTS=${PORTS// /,}
+PORTS=$( IFS=$'\n'; echo "${OPTIONS[@]}")
+PORTS=${PORTS// /,}
 
-#apt-get install iptables -y --force-yes --assume-yes
-#iptables -S >> /machariel/logs/iptables.log
-#mv ./rules.v4 /etc/iptables/rules.v4
-#iptables -I INPUT 1 -i lo -j ACCEPT
-#iptables -I INPUT 2 -p tcp -m tcp -m multiport ! --dports $PORTS -j DROP
-#iptables -A INPUT -j DROP
-#apt-get install iptables-persistent -y --force-yes --assume-yes
-#invoke-rc.d iptables-persistent save
-#mv ./rules.v6 /etc/iptables/rules.v6
+apt-get install iptables -y --force-yes --assume-yes
+iptables -S >> /machariel/logs/iptables.log
+mv ./rules.v4 /etc/iptables/rules.v4
+iptables -I INPUT 1 -i lo -j ACCEPT
+iptables -I INPUT 2 -p tcp -m tcp -m multiport ! --dports $PORTS -j DROP
+iptables -A INPUT -j ACCEPT
+apt-get install iptables-persistent -y --force-yes --assume-yes
+invoke-rc.d iptables-persistent save
+mv ./rules.v6 /etc/iptables/rules.v6
 
 echo "THIS UTILITY IS IN SAFE MODE. UNCOMMENT TO ENABLE."
