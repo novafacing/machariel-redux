3064d37d8a394dada5b489b4e7249bc1ea821711
Rowan Hart <rowanbhart@gmail.com>
2017-09-19 20:11:16 -0400

    Added DOS protection to iptables

--- ubuntuFirewall.sh
+++ ubuntuFirewall.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-while getopts "p:" opt; do
+while getopts "p:i:" opt; do
   case $opt in
     p ) set -f
         IFS=' '
         OPTIONS=($OPTARG);;
+    i ) set -f
+        IFS=' '
+        SOPTIONS=($OPTARG);;
     * )
         clean_up
         exit 1
@@ -12,6 +15,8 @@ while getopts "p:" opt; do
 done
 
 echo $OPTIONS
+echo $SOPTIONS
+echo $WOPTIONS
 
 PORTS=$( IFS=$'\n'; echo "${OPTIONS[@]}")
 PORTS=${PORTS// /,}
@@ -20,10 +25,12 @@ apt-get install iptables -y --force-yes --assume-yes
 iptables -S >> /machariel/logs/iptables.log
 mv ./rules.v4 /etc/iptables/rules.v4
 iptables -I INPUT 1 -i lo -j ACCEPT
-iptables -I INPUT 2 -p tcp -m tcp -m multiport ! --dports $PORTS -j DROP
-iptables -A INPUT -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -m limit --limit 10/second --limit-burst 150 -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate NEW -m limit --limit 10/second --limit-burst 150 -j ACCEPT
+iptables -I INPUT 2 -p tcp -m tcp -m multiport --dports $PORTS -j ACCEPT
+iptables -A INPUT -j DROP
 apt-get install iptables-persistent -y --force-yes --assume-yes
-invoke-rc.d iptables-persistent save
+invoke-rc.d iptables-persistent savel
 mv ./rules.v6 /etc/iptables/rules.v6
 
 echo "THIS UTILITY IS IN SAFE MODE. UNCOMMENT TO ENABLE."
